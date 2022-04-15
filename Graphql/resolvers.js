899d680ef25643e49daf5efa39a501d1218322d2
GenossenCoder <80360252+GenossenCoder@users.noreply.github.com>
2022-04-15 20:48:43 +0200

    year

--- Graphql/resolvers.js
+++ Graphql/resolvers.js
@@ -7,7 +7,7 @@ const Post = require('.././models/Post');
 const User = require('.././models/User');
 const {validateRegisterInput,validateLoginInput} = require('../util/validators')
 const checkAuth = require('../util/user.auth')
-const {SECRET_KEY} = require('../config')
+const SECRET_KEY = process.env.SECRET_KEY
 
 function jwtinit(user){
     return jwt.sign({
--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const mongoose = require('mongoose')
 const typeDefs = require('./Graphql/typeDefs')
 const resolvers = require('./Graphql/resolvers')
 
+const PORT = process.env.PORT || 4000
 
 const server =new ApolloServer({
     typeDefs,
     resolvers,
     context: ({req})=>({req})
 });
-mongoose.connect('mongodb+srv://adim:WknSllcgkPnBuuR1@cluster0.uw8iy.mongodb.net/Talk_Active?retryWrites=true&w=majority',{useNewURLParser:true})
+mongoose.connect(process.env.MONGOURI,{useNewURLParser:true})
     .then(()=>{
         console.log('mongodb')
-        return server.listen({port:5000})
+        return server.listen(PORT)
     }).then(res=>{
         console.log(`listening on port ${res.url}`)
     })
