6f5a6e262ff4c6375fe2604a937822f848438b84
GenossenCoder <80360252+GenossenCoder@users.noreply.github.com>
2022-04-17 12:00:21 +0200

    Heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {ApolloServer, PubSub}= require('apollo-server')
 const mongoose = require('mongoose')
 
-//sshjdhkahdkjashdjkhakhdkj
+//sshjdhkahdkjashdjkhakhdkjqweqezquizeuiziziW
 const typeDefs = require('./Graphql/typeDefs')
 const resolvers = require('./Graphql/resolvers')
 
